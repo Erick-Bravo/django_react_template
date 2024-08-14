c40fbe3df6c22926793bfe578416aea279bc5c28
Erick-Bravo <erickbrvo@gmail.com>
2024-08-14 07:45:51 -0700

    server bug fix. type incorrect

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny # type: ignore
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = User
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
     

0d590abf72951a0a8c52a16621539b537d72c099
Erick-Bravo <erickbrvo@gmail.com>
2024-08-20 13:16:30 -0700

    NoteSerializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics # type: ignore
 from rest_framework.permissions import IsAuthenticated, AllowAny # type: ignore
 from .serializers import UserSerializer, NoteSerializer
 from .models import Note
@@ -19,6 +19,15 @@ class NoteListCreate(generics.ListCreateAPIView):
             serializer.save(author=self.request.user)
         else:
             print(serializer.errors)
+            
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user) # get Notes from this user
+    
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
